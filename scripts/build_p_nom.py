1f60b1b6c268411734395ce80e245fdfce7472ea
Xiaowei-Z <50118569+Xiaowei-Z@users.noreply.github.com>
2022-04-13 15:04:54 +0200

    Update build_p_nom.py

--- scripts/build_p_nom.py
+++ scripts/build_p_nom.py
@@ -9,23 +9,31 @@ from functions import pro_names
 
 def csv_to_df(csv_name=None):
     
-    df = pd.read_csv(csv_name, index_col=0, header=3, skiprows=[35])
+    df = pd.read_csv(csv_name, index_col=0, header=0)
     
     df = df.apply(pd.to_numeric)
 
     return df['MW'].reindex(pro_names)
     
-def build_coal_p_nom():
+def build_p_nom():
 
-    coal_capacity = csv_to_df(csv_name='data/p_nom/coal_p_nom.csv') #coal power plant capacity + CHP capacity
+    coal_capacity = csv_to_df(csv_name="data/p_nom/coal_p_nom.csv") 
 
-    CHP_capacity = csv_to_df(csv_name='data/p_nom/CHP_p_nom.csv') 
-
-    coal_capacity = coal_capacity - 0.75*CHP_capacity #coal power plant capacity 
+    CHP_capacity = csv_to_df(csv_name="data/p_nom/CHP_p_nom.csv")
+    
+    OCGT_capacity = csv_to_df(csv_name="data/p_nom/OCGT_p_nom.csv")
 
     coal_capacity.name = "coal_capacity"
+    
+    CHP_capacity.name = "CHP_capacity"
+    
+    OCGT_capacity.name = "OCGT_capacity"
 
-    coal_capacity.to_hdf(snakemake.output.outfile, key=coal_capacity.name)
+    coal_capacity.to_hdf(snakemake.output.outfile1, key=coal_capacity.name)
+    
+    CHP_capacity.to_hdf(snakemake.output.outfile2, key=CHP_capacity.name)
+    
+    OCGT_capacity.to_hdf(snakemake.output.outfile3, key=OCGT_capacity.name)
     
 if __name__ == "__main__":
 
@@ -33,6 +41,6 @@ if __name__ == "__main__":
     if 'snakemake' not in globals():
         from vresutils import Dict
         snakemake = Dict()
-        snakemake.output = Dict(outfile="data/p_nom/coal_p_nom.h5")
+        snakemake.output = Dict(outfile1="data/p_nom/coal_p_nom.h5", outfile2="data/p_nom/CHP_p_nom.h5", outfile3="data/p_nom/OCGT_p_nom.h5")
         
-     build_p_nom
+    build_p_nom
