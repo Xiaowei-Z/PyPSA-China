f8c0a78b5c95f5a1a6a27f09d24e093c85a909a6
Xiaowei-Z <50118569+Xiaowei-Z@users.noreply.github.com>
2022-05-09 11:44:50 +0200

    Update functions.py

--- scripts/functions.py
+++ scripts/functions.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: CC0-1.0
 
 import numpy as np
+from scipy import interpolate
 
 pro_names = np.array(['Anhui', 'Beijing', 'Chongqing', 'Fujian', 'Gansu', 'Guangdong',
        'Guangxi', 'Guizhou', 'Hainan', 'Hebei', 'Heilongjiang', 'Henan',
@@ -11,3 +12,13 @@ pro_names = np.array(['Anhui', 'Beijing', 'Chongqing', 'Fujian', 'Gansu', 'Guang
        'Shanghai', 'Shanxi', 'Sichuan', 'Tianjin', 'Xinjiang', 'Tibet',
        'Yunnan', 'Zhejiang'],
       dtype=str)
+
+def HVAC_cost_curve(distance):
+
+  d = np.array([608, 656, 730, 780, 903, 920, 1300])
+  c = 1000 / 7.5 * np.array([5.5, 4.71, 5.5, 5.57, 5.5, 5.5, 5.51])
+
+  c_func = interpolate.interp1d(d, c, fill_value='extrapolate')
+  c_results = c_func(distance)
+
+  return c_results
