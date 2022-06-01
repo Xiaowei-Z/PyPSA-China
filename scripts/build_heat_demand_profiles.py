c09c026f7d5eb3e7f45e85bc8b45342c3fc02c9d
Xiaowei-Z <50118569+Xiaowei-Z@users.noreply.github.com>
2022-06-01 10:23:02 +0200

    Add files via upload

--- scripts/build_heat_demand_profiles.py
+++ scripts/build_heat_demand_profiles.py
@@ -14,10 +14,6 @@ def build_heat_demand_profiles():
 
     cutout = atlite.Cutout('cutouts/China-2020.nc')
 
-
-    #list of grid cells
-    grid_cells = cutout.grid_cells()
-
     pop_matrix = sp.sparse.csr_matrix(pop_map.T)
     index = pop_map.columns
     index.name = "provinces"
@@ -29,12 +25,12 @@ def build_heat_demand_profiles():
                             constant=0.,
                             hour_shift=8.)
     
-    Hd_2020 = hd.T.to_pandas().divide(pop_map.sum())
-    Hd_2020.loc['2016-04-01':'2016-09-30'] = 0
+    Hd_2020 = hd.to_pandas().divide(pop_map.sum())
+    Hd_2020.loc['2020-04-01':'2020-09-30'] = 0
     Hd_2020.loc[:,['Anhui','Sichuan','Yunnan','Chongqing','Guizhou','Guangxi','Hainan','Hubei','Hunan','Guangdong','Jiangxi','Shanghai','Zhejiang','Fujian']]=0
     
 
-    with pd.HDFStore(snakemake.output.outfile, mode='w', complevel=4) as store:
+    with pd.HDFStore(snakemake.output.daily_heat_demand, mode='w', complevel=4) as store:
         store['heat_demand_profiles'] = Hd_2020
 
 
@@ -45,6 +41,6 @@ if __name__ == "__main__":
         from vresutils import Dict
         snakemake = Dict()
         snakemake.input = Dict(infile="data/population/population_gridcell_map.h5")
-        snakemake.output = Dict(outfile="data/heating/daily_heat_demand.h5")
+        snakemake.output = Dict(daily_heat_demand="data/heating/daily_heat_demand.h5")
 
     df = build_heat_demand_profiles()
